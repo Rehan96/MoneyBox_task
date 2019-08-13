863e575b7199c1a584d37922b9c8b07d73be8f90
Rehan Khan <rehanuk@hotmail.co.uk>
2019-08-13 11:46:34 +0100

    Initial Commit

---
+++ MoneyBox ios technical task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyBox ios technical task
+//
+//  Created by Rehan Khan on 13/08/2019.
+//  Copyright © 2019 Rehan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
